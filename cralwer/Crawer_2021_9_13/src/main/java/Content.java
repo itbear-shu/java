053ce705ff823428832ea8b1a12a5bfc95e05d18
xiaoxiong <1610580113@qq.com>
2021-09-13 12:38:59 +0800

    2021-9-13-12-38

---
+++ cralwer/Crawer_2021_9_13/src/main/java/Content.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Content {
+    private Integer number;//第几章
+    private String title;//章节名
+    private Integer word_count;//本章字数
+    private Date update_time;//更新时间
+}
--- cralwer/Crawer_2021_9_13/src/main/java/Novel.java
+++ cralwer/Crawer_2021_9_13/src/main/java/Novel.java
@@ -1,2 +1,12 @@
+import java.util.Date;
+
 public class Novel {
+    private String name;//书名
+    private String writer;//作者名
+    private Integer content_id;//内容的id
+    private String type;//书的类型
+    private String state;//书的完成状态
+    private Integer word_count;//单位：万
+    private String source;//来源
+    private String n_url;//连接
 }
